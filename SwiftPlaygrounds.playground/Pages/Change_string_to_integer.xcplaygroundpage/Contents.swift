ad28e679a92b7491b5a58ecfaea0e919afc70f8e
Kyoungmin Nam <rudalswhdk12@naver.com>
2022-01-22 17:41:41 +0900

    commit

--- SwiftPlaygrounds.playground/Pages/Change_string_to_integer.xcplaygroundpage/Contents.swift
+++ SwiftPlaygrounds.playground/Pages/Change_string_to_integer.xcplaygroundpage/Contents.swift
@@ -1,7 +1,17 @@
+//String 타입에서 혹여나 정수타입 또는 부호가 없다면 nil이 나올수 있으므로 ! 추가
 func solution(_ s:String) -> Int {
-    var result : Int
-    return result
+    return Int(s)!
 }
 
-let str = "-1234"
+//강제 추출하지 않고 구현
+func solution2(_ s:String) -> Int {
+    if let num = Int(s) {
+        return num
+    }
+    return 0
+}
+
+
+var str : String = "-1234"
 print(solution(str))
+print(solution2(str))
